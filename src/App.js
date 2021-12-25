1a7a78a4c4de61194d641c040b83ccae8512f681
Aleksey Borsch <a.borsh@iteco-inno.ru>
2021-12-25 20:06:39 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Input, PageHeader } from "antd";
+import { useState } from 'react';
 
 function App() {
+  
+  const defaultN = 7;
+  const calculate = (nParameter) => nParameter*(nParameter + 1)/2;  
+  const [result, setResult] = useState(calculate(defaultN));  
+
+  const handleInputChange = (e) => {
+    setResult(calculate(Number.parseInt(e.target.value)));
+  }
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageHeader title={`Cумма натуральных чисел`}/>            
     </div>
+    Сумма первых
+    <p/>
+    <Input type="number" size='middle' defaultValue={defaultN} onChange={handleInputChange}/>
+    <p/>
+     натуральных чисел равна {result}
+  </>
   );
 }
 
